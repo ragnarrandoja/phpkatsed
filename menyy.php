b84cf4c12b85c162746473515c50a04a43c2dbbb
Ragnar Randoja <ragnar.randoja@tigu.hk.tlu.ee>
2015-04-02 16:05:56 +0300

    asd

---
+++ menyy.php
@@ -0,0 +1,27 @@
+﻿<!doctype html>
+<html>
+  <head>
+    <title>Menüü proov</title>
+  </head>
+  <body>
+     <select name="maakonnad">
+	   <option>Hiiumaa</option>
+	   <option selected="selected">Läänemaa</option>
+	   <option>Harjumaa</option>
+	 </select>
+	 <form action="?">
+	  <select name="riigivalik">
+	 <?php 
+	   $riigid=array("Soome", "Eesti", "Läti", "Leedu");
+	   $valikunr=2;
+	   if(isSet($_REQUEST["riigivalik"])){$valikunr=intval($_REQUEST["riigivalik"]);}
+	   for($riiginr=0; $riiginr<count($riigid); $riiginr++){
+	      $lisand=($riiginr==$valikunr) ? "selected='selected'" : "";
+	      echo "<option value='$riiginr' $lisand>$riigid[$riiginr]</option>\n";
+	   }
+	 ?>
+	  </select>
+	  <input type="submit" value="sisesta" />
+	 </form>
+  </body>
+</html>
